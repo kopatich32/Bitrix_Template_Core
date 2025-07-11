06fddebb2cc1b6da777c1c40bcb6ed0627807d33
kopatich <kopatich@itescort.ru>
2025-07-11 10:03:42 +0300

    Материалы нового шаблона

---
+++ Ex11_NEW_TrainingMaterials/Ex11_TrainingMaterials/demo/search/index.php
@@ -0,0 +1,41 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Поиск");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:search.page",
+	"",
+	Array(
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_SHADOW" => "Y",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "N",
+		"DEFAULT_SORT" => "rank",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FILTER_NAME" => "",
+		"NO_WORD_LOGIC" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => "modern",
+		"PAGER_TITLE" => "Результаты поиска",
+		"PAGE_RESULT_COUNT" => "6",
+		"PATH_TO_USER_PROFILE" => "",
+		"RATING_TYPE" => "",
+		"RESTART" => "N",
+		"SHOW_ITEM_DATE_CHANGE" => "N",
+		"SHOW_ITEM_TAGS" => "N",
+		"SHOW_ORDER_BY" => "N",
+		"SHOW_RATING" => "",
+		"SHOW_TAGS_CLOUD" => "N",
+		"SHOW_WHEN" => "N",
+		"SHOW_WHERE" => "N",
+		"USE_LANGUAGE_GUESS" => "Y",
+		"USE_SUGGEST" => "N",
+		"USE_TITLE_RANK" => "N",
+		"arrFILTER" => array("no")
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
